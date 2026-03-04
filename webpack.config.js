f6ac0be5ae746db274639e8305b15873ba1606ee
wouterverwoerd <wouter.verwoerd@gmail.com>
2026-03-04 15:18:04 +1300

    update source

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: 'https://wouteradvicenode.onrender.com'
         })
     }
 }
